07898fa70018042ffd65ed512e3383721d37fa3e
Aleh Kasiankou <oleg.kosenkov@innowise-group.com>
2023-02-06 20:31:28 +0300

    init commit

---
+++ Innowise.Clinic.Gateway/Program.cs
@@ -0,0 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+builder.Configuration.AddJsonFile(
+    "ocelot.json");
+
+var app = builder.Build();
+
+app.UseOcelot().Wait();
+
+app.Run();
